93597bd6f40d76049684e993d85f30d63d29feba
Iury Krieger <iurykrieger96@gmail.com>
2020-08-25 09:02:48 -0300

    feat: monitor manager

--- boot.sh
+++ boot.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-# Bootstrap file to start the whole configuration
+# Install dependencies
+
+# Setup links
+ln -s $(pwd)/.profile ~/.profile
+ln -s $(pwd)/.config/i3 ~/.config/i3
+ln -s $(pwd)/.config/i3blocks ~/.config/i3blocks
